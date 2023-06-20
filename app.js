b8cd82da953544822287e309d87e422fb7bc16e8
SimplyEly <elydreaves@hotmail.co.uk>
2023-06-20 11:36:37 +0100

    backend

---
+++ app.js
@@ -0,0 +1,28 @@
+// const express = require("express");
+import express from "express";
+import cors from "cors";
+
+import { getQuestions } from "./controllers/math_questions.js"
+
+const app = express();
+
+app.use(cors({}));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+}); 
+
+// app.get at route /math_questions
+    // to res entire array of questions from ../data/math_questions.json
+
+app.get("/math_questions", async function (req, res){
+    const Questions = await getQuestions();
+    res.send(Questions)
+});
+
+
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ controllers/math_questions.js
@@ -0,0 +1,9 @@
+import fs from "node:fs/promises";
+
+const filename = "./data/math_questions.json";
+
+export async function getQuestions() {
+  const data = await fs.readFile(filename);
+  const questions = JSON.parse(data);
+  return questions;
+}
