1b4f403ea36a4f726bd7ae440869f660d0a3ff9b
SimplyEly <elydreaves@hotmail.co.uk>
2023-06-21 13:57:00 +0100

    updated controllers

--- app.js
+++ app.js
@@ -2,27 +2,25 @@
 import express from "express";
 import cors from "cors";
 
-import { getQuestions } from "./controllers/math_questions.js"
+import { getQuestions, tenQuestions, tenRandomQuestions } from "./controllers/math_questions.js";
 
 const app = express();
 
 app.use(cors({}));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-}); 
-
-// app.get at route /math_questions
-    // to res entire array of questions from ../data/math_questions.json
-
-app.get("/math_questions", async function (req, res){
-    const Questions = await getQuestions();
-    res.send(Questions)
+  res.send("Hello World!");
 });
 
+// app.get at route /math_questions
+// to res entire array of questions from ../data/math_questions.json
 
+app.get("/math_questions", async function (req, res) {
+  const Questions = await tenRandomQuestions();
+  res.send(Questions);
+});
 
-const port = 3001
+const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
--- controllers/math_questions.js
+++ controllers/math_questions.js
@@ -7,3 +7,27 @@ export async function getQuestions() {
   const questions = JSON.parse(data);
   return questions;
 }
+
+export async function tenQuestions() {
+  const data = await fs.readFile(filename);
+  const questions = JSON.parse(data);
+  const i = Math.floor(Math.random() * questions.length);
+  console.log(i);
+  const tenQuestions = questions.slice(i, i + 10);
+  console.table(tenQuestions);
+  return tenQuestions;
+}
+
+export async function tenRandomQuestions() {
+  const data = await fs.readFile(filename);
+  const tenRandomQuestions = [];
+  for(let i = 0; i < 3; i++) {
+    const questions = JSON.parse(data);
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+    console.log(randomQuestion);
+    tenRandomQuestions.push(randomQuestion);
+    console.log(tenRandomQuestions);
+  }
+  return tenRandomQuestions;
+}
